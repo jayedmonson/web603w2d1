097aa3cf62e67c57c9c5662bb841aef449e45ea5
jayedmonson <143557802+jayedmonson@users.noreply.github.com>
2024-01-17 21:53:37 -0600

    in class complete

--- demo4.js
+++ demo4.js
@@ -1,18 +1,23 @@
 class FileInput extends React.Component {
     constructor(props) {
-    
       super(props);
-      
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.fileInput = React.createRef();
     }
     handleSubmit(event) {
-      
+      alert(
+        `Selected file - ${this.fileInput.curent.files[0].name}`
+      );
     }
   
     render() {
    
       return (
         <form onSubmit={this.handleSubmit}>
-          
+          <label>
+            Upload file: 
+            <input type="file" ref={this.fileInput} />
+          </label>
           <br />
           <button type="submit">Submit</button>
         </form>
